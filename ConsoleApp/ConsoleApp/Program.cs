9416fcd23833fb255b27e7aa80e030b77d300c0f
Daniel Sc <daniel.scepanovic@iths.se>
2022-09-07 10:00:39 +0200

    regex

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,107 +1,34 @@
-﻿namespace ConsoleApp
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            string InputChars = "29535123p48723487597645723645";
-            string validChars = "123456789";
+﻿using System.Text.RegularExpressions;
 
-            for (int i = 0; i < InputChars.Length; i++) //InputChars loop
-            {
-                for (int k = 0; k < validChars.Length; k++) // validChars loop
-                {
-                    if (InputChars[i] == validChars[k]) 
-                    {
-                        int index = InputChars.IndexOf(validChars[k], i + 1);
-                        if (index == -1) continue;
-                        string match = InputChars.Substring(i, index - i + 1);
-
-                        foreach (char c in match)
-                        {
-                            if (!Char.IsDigit(c))
-                            {
-                                bool nonDigit = true;
-                            }
-                        }
-
-                        /*
-                        foreach (char c in match)
-                        {
-                            if (!Char.IsDigit(c))
-                            {
-                                goto LoopEnd;
-                            }
-                        }
-                        */
-                        Console.Write(InputChars);
-                        Console.SetCursorPosition(i, Console.CursorTop);
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine(match);
-                        Console.ForegroundColor = ConsoleColor.White;
-                        LoopEnd:;
-                    }
-                }
-            }
-        }
-    }
-}
-
-/*
 namespace ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string numbers = "0123456789";
             string input = "29535123p48723487597645723645";
-            string output = string.Empty;
-            int firstIndex = 0;
-            int lastIndex = -1;
-            int sum = 0;
-            string sumString = string.Empty;
+            string inputSub = input;
 
-            for(int i = 0; i < numbers.Length; i++)
+            for (int i = 0; i < input.Length; i++)
             {
-                firstIndex = 0;
-                lastIndex = -1;
-                while (true)
+                Regex rx = new Regex($@"^{inputSub[0]}\d+?{inputSub[0]}");
+                string match = rx.Match(inputSub).Value;
+
+                inputSub = inputSub.Substring(1, inputSub.Length - 1);
+
+                if (match == "")
                 {
-                    firstIndex = input.IndexOf(numbers[i], lastIndex + 1);
-                    if (firstIndex == -1) break;
-
-                    lastIndex = input.IndexOf(numbers[i], firstIndex + 1);
-                    if (lastIndex == -1) break;
-
-                    for (int j = firstIndex; j <= lastIndex; j++)
-                    {
-                        output += Convert.ToString(input[j]);
-                        if (numbers.IndexOf(input[j]) == -1)
-                        {
-                            break;
-                        }
-                    }
-
+                    continue;
+                }
+                else
+                {
                     Console.Write(input);
-
-                    Console.SetCursorPosition(firstIndex, Console.CursorTop);
-
+                    Console.SetCursorPosition(i, Console.CursorTop);
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Write(output);
+                    Console.WriteLine(match);
                     Console.ForegroundColor = ConsoleColor.White;
-
-                    Console.WriteLine("");
-                    sumString += output;
-                    output = "";
                 }
             }
-            for(int i = 0; i < sumString.Length; i++)
-            {
-                sum += Convert.ToInt32(sumString[i]);
-            }
-            Console.WriteLine(sum);
         }
     }
 }
-*/
